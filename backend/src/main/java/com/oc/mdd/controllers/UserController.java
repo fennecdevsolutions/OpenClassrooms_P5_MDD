560cd17bf69bdb6f37d13e5e478ee14553dc0b45
Fennec Dev <fennec.dev.solutions@gmail.com>
2026-03-10 13:09:22 +0100

    add email and password validation; add validation started dependency; add validation exception handling;

--- backend/src/main/java/com/oc/mdd/controllers/UserController.java
+++ backend/src/main/java/com/oc/mdd/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oc.mdd.dto.ErrorResponseDto;
 import com.oc.mdd.dto.JwtTokenDto;
 import com.oc.mdd.dto.RegisterRequestDto;
 import com.oc.mdd.services.UserService;
@@ -17,6 +18,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -28,8 +30,10 @@ public class UserController {
 
 	@Operation(summary = "Register a new user", description = "Creates a new user account and returns a JWT token upon success.")
 	@ApiResponse(responseCode = "201", description = "User registered successfully", content = @Content(schema = @Schema(implementation = JwtTokenDto.class)))
+	@ApiResponse(responseCode = "409", description = "Username or Email already registered", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+	@ApiResponse(responseCode = "400", description = "Registration form invalid", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
 	@PostMapping("/auth/register")
-	public ResponseEntity<JwtTokenDto> registerUser(@RequestBody RegisterRequestDto registerRequest) {
+	public ResponseEntity<JwtTokenDto> registerUser(@Valid @RequestBody RegisterRequestDto registerRequest) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(registerRequest));
 	}
 
--- backend/src/main/java/com/oc/mdd/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/oc/mdd/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -34,6 +35,13 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<ErrorResponseDto> handleUnexpectedException(Exception ex, WebRequest request) {
 		logger.error("SYSTEM ERROR: An unhandled exception occurred at {}", request.getDescription(false), ex);
 		return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected server error has occured", request);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException ex,
+			WebRequest request) {
+		String firstErroMsg = ex.getBindingResult().getFieldError().getDefaultMessage();
+		return buildResponse(HttpStatus.BAD_REQUEST, firstErroMsg, request);
 	}
 
 	private ResponseEntity<ErrorResponseDto> buildResponse(HttpStatus status, String message, WebRequest request) {
