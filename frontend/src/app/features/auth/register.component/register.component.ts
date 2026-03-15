465b9a881f0d43402a7cc40e8beee9f4cf5d21d1
Fennec Dev <fennec.dev.solutions@gmail.com>
2026-03-15 12:04:51 +0100

    update register component; update global styles and home component styles;

--- frontend/src/app/features/auth/register.component/register.component.ts
+++ frontend/src/app/features/auth/register.component/register.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterLink,
+    MatIconModule,
+    MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  serverErrorMessage: string | null = null;
+
+
+  registerForm = new FormGroup({
+    username: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+    password: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])/)] })
+  });
+
+  onSubmitForm() {
+
+    console.log("Sending data to server", this.registerForm.value);
+    this.serverErrorMessage = "Testing the server error message"
+  }
+
+
+}
